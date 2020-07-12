1f628f98abfdc2a15e3e03fffd193cabaa25d6ee
Miguel Sanchez <msanchez@fidenslat.com>
2020-07-11 21:51:46 -0500

    [UPD] Libraries, [DEL] sass, [UPD] gulp file dont process css, [UPD] Resume info

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const browsersync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
 const del = require("del");
 const gulp = require("gulp");
-const header = require("gulp-header");
 const merge = require("merge-stream");
 const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
@@ -16,14 +15,6 @@ const uglify = require("gulp-uglify");
 // Load package.json for banner
 const pkg = require('./package.json');
 
-// Set the banner content
-const banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
-].join('');
 
 // BrowserSync
 function browserSync(done) {
@@ -71,29 +62,26 @@ function modules() {
 }
 
 // CSS task
-function css() {
-  return gulp
-    .src("./scss/**/*.scss")
-    .pipe(plumber())
-    .pipe(sass({
-      outputStyle: "expanded",
-      includePaths: "./node_modules",
-    }))
-    .on("error", sass.logError)
-    .pipe(autoprefixer({
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest("./css"))
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("./css"))
-    .pipe(browsersync.stream());
-}
+// function css() {
+//   return gulp
+//     .src("./scss/**/*.scss")
+//     .pipe(plumber())
+//     .pipe(sass({
+//       outputStyle: "expanded",
+//       includePaths: "./node_modules",
+//     }))
+//     .on("error", sass.logError)
+//     .pipe(autoprefixer({
+//       cascade: false
+//     }))
+//     .pipe(gulp.dest("./css"))
+//     .pipe(rename({
+//       suffix: ".min"
+//     }))
+//     .pipe(cleanCSS())
+//     .pipe(gulp.dest("./css"))
+//     .pipe(browsersync.stream());
+// }
 
 // JS task
 function js() {
@@ -103,9 +91,6 @@ function js() {
       '!./js/*.min.js'
     ])
     .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
     .pipe(rename({
       suffix: '.min'
     }))
@@ -115,18 +100,18 @@ function js() {
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./scss/**/*", css);
+  // gulp.watch("./scss/**/*", css);
   gulp.watch(["./js/**/*", "!./js/**/*.min.js"], js);
   gulp.watch("./**/*.html", browserSyncReload);
 }
 
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
+const build = gulp.series(vendor, gulp.parallel(js));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
-exports.css = css;
+// exports.css = css;
 exports.js = js;
 exports.clean = clean;
 exports.vendor = vendor;
